27bab0fc81339b4fa085f160685c7af63108a089
Aznim Rahman <aznimr@protonmail.com>
2025-05-12 13:57:24 +0100

    update link component

--- lib/components/Link/Link.tsx
+++ lib/components/Link/Link.tsx
@@ -1,39 +1,47 @@
 import { LinkIcon } from "@/lib/components/icons";
 import { cn } from "@/lib/utils";
-import { ComponentProps, ReactNode } from "react";
+import { ComponentProps, forwardRef, ReactNode } from "react";
 
 interface LinkProps extends ComponentProps<"a"> {
   isExternal?: boolean;
   showLinkIcon?: boolean;
   customIcon?: ReactNode;
 }
+export const Link = forwardRef<HTMLAnchorElement, LinkProps>(
+  (
+    {
+      children,
+      className,
+      isExternal = false,
+      showLinkIcon = false,
+      customIcon,
+      href,
+      ...props
+    },
+    ref
+  ) => {
+    const icon = customIcon ?? (
+      <LinkIcon className="transition-all duration-200 group-hover:rotate-[360deg]" />
+    );
 
-export const Link = ({
-  children,
-  className,
-  isExternal = false,
-  showLinkIcon = false,
-  customIcon,
-  href,
-  ...props
-}: LinkProps) => {
-  const icon = customIcon ?? (
-    <LinkIcon className="transition-all duration-200 group-hover:rotate-[360deg]" />
-  );
+    return (
+      <a
+        ref={ref}
+        href={href}
+        target={isExternal ? "_blank" : undefined}
+        rel={isExternal ? "noopener noreferrer" : undefined}
+        className={cn(
+          "group inline-flex items-center gap-1.5 text-blue-500 hover:text-opacity-85 transition-all duration-200",
+          className
+        )}
+        role="link"
+        {...props}
+      >
+        {children}
+        {showLinkIcon && icon}
+      </a>
+    );
+  }
+);
 
-  return (
-    <a
-      href={href}
-      target={isExternal ? "_blank" : undefined}
-      rel={isExternal ? "noopener noreferrer" : undefined}
-      className={cn(
-        "group inline-flex items-center gap-1.5 text-blue-500 hover:transition-all hover:text-opacity-85 duration-200",
-        className
-      )}
-      {...props}
-    >
-      {children}
-      {showLinkIcon && icon}
-    </a>
-  );
-};
+Link.displayName = "Link";
